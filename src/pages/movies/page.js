2b1ebba9e892abd48602e87e9f6ff270ff643522
FacuTombesi <facundomtombesi@gmail.com>
2024-05-14 15:36:42 -0300

    add(-): added nav bar to movies page

--- src/pages/movies/page.js
+++ src/pages/movies/page.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Link from "next/link";
+import Nav from "@/components/Nav";
 
 export default function Movies() {
   const [movies, setMovies] = useState([])
@@ -27,7 +28,10 @@ export default function Movies() {
 
   return (
     <div className="flex flex-col items-center justify-between min-h-screen">
-      <Header />
+      <div>
+        <Header />
+        <Nav />
+      </div>
       {!loading && (
         <div className="grid xl:grid-cols-6 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 lg:gap-6 sm:gap-3 justify-center my-20">
           {movies.map((movie) => (
@@ -35,7 +39,7 @@ export default function Movies() {
               <Link href={`/movies/${movie.url.split("/").slice(-2)[0]}`}>
                 <div className="flex flex-col items-center p-6">
                   <img src="/images/generic_movie.jpg" alt={movie.title} className="h-80 w-auto mb-4" />
-                  <h2 className="text-center font-bold">{movie.title}</h2>
+                  <h2 className="text-center font-bold uppercase">{movie.title}</h2>
                   <p className="text-center text-sm">Episode {movie.episode_id}</p>
                 </div>
               </Link>

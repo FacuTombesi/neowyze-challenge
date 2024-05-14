24c4b3b9e10fda4e40ef20dbf9fbcc7572de12e2
FacuTombesi <facundomtombesi@gmail.com>
2024-05-14 18:13:01 -0300

    fix(-): fixed footer text size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="flex flex-col items-center justify-center text-center py-4">
-      <p className="text-sm">Star Wars themed challenge for Neowyze by Facundo Tombesi</p>
+      <p className="text-xs">Star Wars themed challenge for Neowyze by Facundo Tombesi</p>
       <Link href="https://ftportfolio.vercel.app/" target="_blank">
         <img src="/images/logo_ft.png" alt="Facundo Tombesi logo" className="w-12" />
       </Link>

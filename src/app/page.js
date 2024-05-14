e4fd6432cedc5d61fbe7671bab6b332b40220f56
FacuTombesi <facundomtombesi@gmail.com>
2024-05-14 14:05:56 -0300

    fix(-): fixed landing page styles

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-between min-h-screen">
       <Header />
       <h1>Neowyze Challenge</h1>
       <Footer />
